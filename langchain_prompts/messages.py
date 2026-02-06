ecfe228e2c895def6427e5a984c66749323874e3
Harsh-nandanshukla <b23mt1019@iitj.ac.in>
2026-02-07 00:12:10 +0530

    Add lagchain_prompts

---
+++ langchain_prompts/messages.py
@@ -0,0 +1,13 @@
+from langchain_core.messages  import SystemMessage,HumanMessage,AIMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+model=ChatOpenAI()
+messages=[
+    SystemMessage(content='You are a helpful assistant'),
+    HumanMessage(content='tell me about langchain in  5 lines')
+
+]
+result=model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+print(messages)
